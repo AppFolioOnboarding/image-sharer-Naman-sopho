a2c0c8f5e83e138a8acb4fe013c240301eefab7d
Naman TIwari <naman.tiwari@appfolio.com>
2021-03-25 09:56:42 -0700

    Implement feedback form backend.

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,11 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-      # Implement your create action
+      Feedback.create!(feedback_params)
+    end
+
+    def feedback_params
+      params.require(:feedback).permit(:name, :comment)
     end
   end
 end
--- test/models/feedback_test.rb
+++ test/models/feedback_test.rb
@@ -1,4 +1,21 @@
 require 'test_helper'
 
 class FeedbackTest < ActiveSupport::TestCase
+  def test_feedback_record_validation
+    feedback = Feedback.new(name: 'Test', comment: 'Comment')
+    assert feedback.valid?
+  end
+
+  def test_feedback_record_validation__no_input
+    feedback = Feedback.new(name: '', comment: '')
+    assert_not feedback.valid?
+  end
+
+  def test_feedback_record_validation__invalid_input
+    feedback = Feedback.new(name: 'Test')
+    assert_not feedback.valid?
+
+    feedback = Feedback.new(comment: 'comment')
+    assert_not feedback.valid?
+  end
 end
